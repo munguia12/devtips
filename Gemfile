b4c14035fed23a61e4bf08551b82de160e07e27b
Santiago Munguia <skandalers@yahoo.com.mx>
2016-07-07 16:01:30 -0500

    three compile

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '>= 5.0.0.rc1', '< 5.1'
 # Use Puma as the app server
+gem "sqlite3"
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require_relative 'boot'
-
+config.assets.initialize_on_precompile = false
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module Inventarios
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
