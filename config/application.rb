56071a276d7d590859bdd20be5410383152d8e8f
Santiago Munguia <skandalers@yahoo.com.mx>
2016-07-07 16:14:18 -0500

    lol

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-config.assets.initialize_on_precompile = false
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Inventarios
   class Application < Rails::Application
+  	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
